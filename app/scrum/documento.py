32d53ed39235870a51471a0142d47335e5aefd4d
Marcos Campos <whosthemark@gmail.com>
2016-02-16 01:02:56 -0430

    Added working file upload example It doesn't use Flask-Upload library

--- app/scrum/documento.py
+++ app/scrum/documento.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import request, session, Blueprint, json, Flask, render_template
-from flask.ext import uploads
+from flask import request, Blueprint, Flask, render_template
 
 app = Flask(__name__)
 documento = Blueprint('documento', __name__)
 app.config['UPLOADED_FILES_DEST'] = 'uploadedFiles/'
-files = uploads.UploadSet('files', uploads.ALL)
-uploads.configure_uploads(app, files)
+
 
 @documento.route('/testUpload')
 def index():
@@ -20,5 +18,4 @@ def upload():
     """Upload a new file."""
     file = request.files['file']
     file.save(os.path.join(app.config['UPLOADED_FILES_DEST'], file.filename))
-    #files.save(file, name = file.filename)
-    return url_for('index')
+    return 'Archivo subido en: ' + app.config['UPLOADED_FILES_DEST'] + file.filename
