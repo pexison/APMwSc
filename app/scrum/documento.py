77fc7b7969584daed9be3f335d3c514dc7db7f50
Marcos Campos <whosthemark@gmail.com>
2016-02-16 00:50:11 -0430

    Added not-so-functional Flask-upload lib example

--- app/scrum/documento.py
+++ app/scrum/documento.py
@@ -2,7 +2,7 @@
 
 import os
 from flask import request, session, Blueprint, json, Flask, render_template
-from flask.ext import  uploads
+from flask.ext import uploads
 
 app = Flask(__name__)
 documento = Blueprint('documento', __name__)
@@ -18,5 +18,7 @@ def index():
 @documento.route('/upload', methods=['POST'])
 def upload():
     """Upload a new file."""
-    files.save(request._files['upload'], author = 'ProyectoDePrueba', name = name)
+    file = request.files['file']
+    file.save(os.path.join(app.config['UPLOADED_FILES_DEST'], file.filename))
+    #files.save(file, name = file.filename)
     return url_for('index')
