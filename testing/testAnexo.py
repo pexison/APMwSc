b2756bf2357d11dfeeba95ec43c813b969f0d1c0
John <pexison@gmail.com>
2016-02-24 20:48:05 -0430

    Test file for the anexo.py created

---
+++ testing/testAnexo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-.
+
+import sys
+import unittest
+
+# Ruta que permite utilizar el módulo anexo.py
+sys.path.append('../app/scrum')
+
+from anexo import *
+
+class TestAnexo(unittest.TestCase):
+
+    #############################################
+    #         Pruebas para insertAnexo          #
+    #############################################
+
+
