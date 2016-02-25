0dc5464c5c7c4cec156ccef90c90a315f975389c
Carlos Farinha <09-10270@usb.ve>
2016-02-24 21:14:29 -0430

    Update testAnexo.py

--- testing/testAnexo.py
+++ testing/testAnexo.py
@@ -15,3 +15,39 @@ class TestAnexo(unittest.TestCase):
     #############################################
 
 
+#Para Carga
+	Casos Bordes
+		-Verificar que el archivo se pueda cargar con el nombre correspondiente
+		-Archivo posee Mayuscula
+		-Archivo posee minusculas
+		-Archivo posee digitos
+		-Archivo nombre vacio
+		-Archivo nombre muy largo
+
+	Casos Esquina
+		-Sin Backlog
+		-Descripcion de 140 o mas sin Backlog
+
+	Casos maliciosos
+		-El String no existe
+		-Simbolos que representan PATH en nuestro SO
+		-Archivos con caracteres reservados a directorio
+
+Para Search
+	Casos Bordes
+		-Si el archivo existe
+		-Si el archivo no existe
+		-El Id del archivo es invalido
+		-Valor del Id negativo
+
+	Maliciosos
+		-Verificar si la busqueda es invalida
+
+
+Para Delete
+	Casos Normal
+		- El archivo existe y se borra exitosamente
+		- El archivo no existe
+		- Longuitud de archivo (grande y pequeña)
+		- Backlog invalido
+#
