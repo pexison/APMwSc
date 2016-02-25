20bcb06cb0266b1e99a778385c7bf767f3492cab
John <pexison@gmail.com>
2016-02-24 21:40:39 -0430

    tests for anexo.py defined

--- testing/testAnexo.py
+++ testing/testAnexo.py
@@ -14,40 +14,80 @@ class TestAnexo(unittest.TestCase):
     #         Pruebas para insertAnexo          #
     #############################################
 
+    # Caso inicial
+    #Verificar que el archivo se pueda cargar con el nombre correspondiente
+    # Prueba 1
 
-#Para Carga
-	Casos Bordes
-		-Verificar que el archivo se pueda cargar con el nombre correspondiente
-		-Archivo posee Mayuscula
-		-Archivo posee minusculas
-		-Archivo posee digitos
-		-Archivo nombre vacio
-		-Archivo nombre muy largo
+    # Casos Normales
+    #Verificar que el archivo se pueda cargar con el nombre correspondiente
+    # Prueba 2
 
-	Casos Esquina
-		-Sin Backlog
-		-Descripcion de 140 o mas sin Backlog
+    # Casos fronteras
+    #Archivo posee Mayuscula
+    # Prueba 3
+    #Archivo posee minusculas
+    # Prueba 4
+    #Archivo posee digitos
+    # Prueba 5
+    #Archivo posee combinacion de todas las anteriores
+    # Prueba 6
+    #Archivo nombre muy largo
+    # Prueba 7
 
-	Casos maliciosos
-		-El String no existe
-		-Simbolos que representan PATH en nuestro SO
-		-Archivos con caracteres reservados a directorio
+    # Casos esquina
+    #Sin Backlog
+    # Prueba 8
+    #Descripcion de 140 o mas sin Backlog
+    # Prueba 9
 
-Para Search
-	Casos Bordes
-		-Si el archivo existe
-		-Si el archivo no existe
-		-El Id del archivo es invalido
-		-Valor del Id negativo
+    # Casos maliciosos
+    #Simbolos que representan PATH en nuestro SO
+    # Prueba 10
+    #Archivo nombre vacio
+    # Prueba 11
 
-	Maliciosos
-		-Verificar si la busqueda es invalida
+    #############################################
+    #         Pruebas para searchAnexo          #
+    #############################################
+
+    # Caso inicial
+    # Busqueda exitosa
+    # Prueba 11
+
+    # Casos normales
+    #Si el archivo existe
+    # Prueba 12
+    #Si el archivo no existe
+    # Prueba 13
+
+    # Casos maliciosos
+    #El Id del archivo es invalido
+    # Prueba 14
+    #Valor del Id negativo
+    # Prueba 15
 
 
-Para Delete
-	Casos Normal
-		- El archivo existe y se borra exitosamente
-		- El archivo no existe
-		- Longuitud de archivo (grande y pequeña)
-		- Backlog invalido
-#
+    #############################################
+    #         Pruebas para deleteAnexo          #
+    #############################################
+
+    # Caso inicial
+    # Prueba 16
+
+    # Casos Normales
+    # El archivo existe y se borra exitosamente
+    # Prueba 17
+
+    # Casos Esquinas
+    # Backlog invalido
+    # Prueba 18
+
+    # Casos fronteras
+    # Longuitud de archivo (grande y pequeña)
+    # Prueba 19
+    # Prueba 20
+
+    # Casos maliciosos
+    # Entrada vacia
+    # Prueba 21
+
